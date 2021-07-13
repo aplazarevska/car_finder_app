5281358a41c2c138a89bfd6783e062ea99259783
Ana Lazarevska <aplazarevska@gmail.com>
2021-07-13 10:18:00 +1000

    images added

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ApplicationRecord
   # belongs_to :profile
-
-  belongs_to :buyer, class_name: "Profile", optiona: true
+  belongs_to :buyer, class_name: "Profile", optional: true
   belongs_to :seller, class_name: "Profile"
+  
+  has_many_attached :pictures
 end
